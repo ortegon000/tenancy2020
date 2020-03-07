8ac8c0a23933970ea5d7740e02aa0a53daa3cc2c
Eduardo Ortega <ortegon000@hootmail.com>
2020-03-06 18:12:21 -0600

    little test change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Controlmas.MX...
+                    @if ( $hostname = app(Environment::class)->hostname() )
+                        {{ $hostname->$hostname }}
+                    @else
+                        Controlmas.MX
+                    @endif
                 </div>
 
                 <div class="links">
